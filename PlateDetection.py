ea8382607c6f4d6d8e54c573cc2003cd7e0a3285
SEB-HDEZ-G <SEB-HDEZ-G@users.noreply.github.com>
2025-05-06 08:37:31 -0600

    if applied, this commit will fix comment structure
    Comments now all start with capital letter (unless continuations) and end with a period (unless custom titles)

--- PlateDetection.py
+++ PlateDetection.py
@@ -4,11 +4,11 @@ from matplotlib import pyplot as plt
 import easyocr
 
 
-#Gaussian filter function
+# Gaussian filter function
 def gaussianFilter(img, kernelSize, sigma=0):
     """
     The Gaussian filter talkes as parameters:
-    - img: Input image (can be grayscale or color, but I'm using grayscale)
+    - img: Input image (can be grayscale or color, but I'm using color)
     - kernelSize: Size of the Gaussian kernel (must be odd), larger kernel
       gives a stronger blur
     - sigma: Standard deviation (how much smoothing is applied), gets
@@ -18,54 +18,55 @@ def gaussianFilter(img, kernelSize, sigma=0):
     return cv.GaussianBlur(img, (kernelSize, kernelSize), sigma)
 
 
-#STEP 1 - image reading
+# STEP 1 - image reading
 img = cv.imread("./ImageTextDetector/placa_q.jpg")
 img2 = cv.imread("./ImageTextDetector/placa_2.jpg")
 imgGroup = [img, img2]
-#grayscale conversion can't be used for HSV color filtering, but
-# cv.IMREAD_GRAYSCALE would be used otherwise
+# Grayscale conversion can't be used for HSV color filtering, but
+# cv.IMREAD_GRAYSCALE would be used otherwise.
 
-#STEPS 2 & 3 - filter white from image and apply Gaussian filter
+# STEPS 2 & 3 - filter white from image and apply Gaussian filter
 for i, img in enumerate(imgGroup):
-    #noise reduction using Gaussian filter, kernel size of 39
+    # Noise reduction using Gaussian filter, kernel size of 39.
     imgGaussian = gaussianFilter(img, 39)
     
     imgHSV = cv.cvtColor(imgGaussian, cv.COLOR_BGR2HSV)
 
-    #color range, filter white
+    # Color range, filter white.
     lowerBound = np.array([0, 0, 200])
     upperBound = np.array([180, 30, 255])
     
-    #binary mask, white pixels (255) represent the color within the defined
-    # range, black pixels (0) represent the rest
+    # Binary mask, white pixels (255) represent the color within the defined
+    # range, black pixels (0) represent the rest.
     mask = cv.inRange(imgHSV, lowerBound, upperBound)
     
-    #image filtering, applies the mask to the original image, keeps only the
-    # pixels that fall within the color range
+    # Image filtering, applies the mask to the original image, keeps only the
+    # pixels that fall within the color range.
     imgFiltered = cv.bitwise_and(img, img, mask=mask)
     
-    #store image
+    # Store image.
     cv.imwrite(f"filteredBlackImg{i+1}.jpg", imgFiltered)
 
 #STEP 4 - reading the text from the image with easyOCR
-reader = easyocr.Reader(["es"]) #spanish
-reader2 = easyocr.Reader(["es"]) #spanish
+reader = easyocr.Reader(["es"]) # Spanish
+reader2 = easyocr.Reader(["es"]) # Spanish
 
-#read text from image
+# Read text from image.
 result = reader.readtext("./ImageTextDetector/filteredBlackImg1.jpg")
 result2 = reader.readtext("./ImageTextDetector/filteredBlackImg2.jpg")
 
-#print the extracted text with confidence rate
+# Print the extracted text with confidence rate.
 for (bbox, text, prob) in result:
     print(f"Text: {text}, Detection confidence: {prob}")
     
-#print the second extracted text with confidence rate
+# Print the second extracted text with confidence rate.
 for (bbox, text, prob) in result2:
     print(f"Text: {text}, Detection confidence: {prob}")
     
-#python convention changes:
-#1 - limited characters per line to 72 characters
-# (only applied for comments and docstrings)
-#2 - surrounded function definitions with two blank lines
-#3 - applied consistency for string quotes
-# (always using double quotes, unless special case)
+# Python convention changes:
+# 1 - limited characters per line to 72 characters
+#     (only applied for comments and docstrings).
+# 2 - surrounded function definitions with two blank lines.
+# 3 - applied consistency for string quotes
+#     (always using double quotes, unless special case).
+# 4 - fixed comment structure, readability and consistency.
